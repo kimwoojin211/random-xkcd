f498cbf51de2361bf07b5e702dd2b2efba45a4e5
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-10 08:31:56 -0800

    add dotenv object

--- src/main.js
+++ src/main.js
@@ -2,3 +2,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+// <!-- comics 1-2422 -->
+$('#Random').click(function () {
+  let currentComic = `http://xkcd.com/info.0.json`
+  const comicNum = Math.floor(Math.random() * currentComic.num);
+  let promise = new Promise(function(resolve,reject){
+    let request = new XMLHttpRequest();
+    const url = `http://xkcd.com/${comicNum}/info.0.json`;
+    request.onload = function () {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(request.response);
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
+  promise.then(function (response) {
+    const body = JSON.parse(response);
+    }, function (error) {
+  });
+});
+
+
+
+// let currentComic = `http://xkcd.com/info.0.json`
+// currentComic.num
